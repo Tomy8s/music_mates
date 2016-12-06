7ce4042398e44e384c44e4d6d6f4e49101b90300
Tomy8s <tomy8s@hotmail.com>
2016-12-06 13:07:54 +0000

    button colours

--- client/layouts/Navigation.js
+++ client/layouts/Navigation.js
@@ -1,4 +1,14 @@
 Template.Navigation.rendered = function() {
   var selectedNavButton = FlowRouter._current.path.substring(1);
-  document.getElementById(selectedNavButton).className = 'selected_route';
+  
 }
+
+Template.Navigation.events({
+  'click nav button'(){
+    var buttonId = event.target.id;
+    FlowRouter.go(buttonId);
+    document.getElementsByClassName('selected_route').className = 'navButton';
+    console.log(document.getElementsByClassName('selected_route'));
+    document.getElementById(buttonId).className = 'selected_route';
+  }
+});
