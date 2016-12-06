33909e4d3d3d4ea48aba9412ac62677b1e63a264
souljuse <oppo.irene@gmail.com>
2016-12-07 00:19:24 +0100

    In friends route one can add a friend from a list of user on the right - CSS added

--- client/Friends/Friends.js
+++ client/Friends/Friends.js
@@ -1,16 +1,17 @@
 import { Template } from 'meteor/templating';
 
 Template.Friends.helpers({
-  userList() {
-    return Meteor.users.find();
-  }
+
 });
+
+Template.Friends.AllUsers = function(){
+    return Meteor.users.find();
+}
 
 Template.Friends.events({
   'click #friend-request-btn'(event) {
     event.preventDefault();
-    var currentUser = Meteor.user();
-    var user = Meteor.users.findOne(currentUser);
+    var user = Meteor.users.findOne(this._id);
     var request = user.requestFriendship();
   },
   'click [data-action=accept]': function() {
