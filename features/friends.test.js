3cbea64a4f3598a1478d89acb4d3d81400872ec2
James <James.Steel@me.com>
2016-12-08 18:26:03 +0000

    friend tests in progress

--- features/friends.test.js
+++ features/friends.test.js
@@ -1,8 +1,12 @@
 import { signUp, signIn, signOut } from './testHelpers.test'
 
 describe('friends', function() {
+
+  context('no friends', function(){
     it('displays "You have no friends", when no friends', function() {
-        signUp('friendTester', 'friend@test.com', 'testpassword');
+        signUp('friendTester1', 'freind1@test.com', 'testpassword');
+        signOut();
+        signUp('friendTester2', 'friend2@test.com', 'testpassword');
         browser.url('http://localhost:3000/friends');
         expect(browser.getText('#friends-list')).to.equal("You don't have any friends yet.");
     });
@@ -11,6 +15,28 @@ describe('friends', function() {
     });
     it('displays message, when no friend requests made', function() {
         expect(browser.getText('#requests-made-list')).to.equal("Your friends have already responded to all your requests.");
+    });
+  });
+
+  context('adding friends', function(){
+    it('shows list of suggested friends', function(){
+         expect(browser.getText('#friend-request-btn')).to.equal('Follow friendTester1')
+    });
+    it('removes from suggested friends on click', function(){
+        browser.click('#friend-request-btn')
+        var doesNotExist = browser.waitForExist('#friend-request-button', undefined, true)
+        expect(doesNotExist).to.equal(true);
+    });
+    it('adds friend request to pending list', function(){
+        expect(browser.getText('#requests-made-list')).to.equal('friendTester1 - Cancel pending request')
         signOut();
     });
+    it('adds friend request to freind request list', function(){
+        signIn('friendTester1', 'testpassword');
+        browser.url('http://localhost:3000/friends');
+        browser.waitForExist('#requests-received-list', 10000);
+        expect(browser.getText('#requests-received-list')).to.equal('friendTester2 would like to be friends');
+        signOut();
+    });
+  });
 });
