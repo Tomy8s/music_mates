13cd12cef16adb4972d0f1ab6e60470f62ce89e8
souljuse <oppo.irene@gmail.com>
2016-12-09 16:58:14 +0100

    image method

--- client/Profile/profile.js
+++ client/Profile/profile.js
@@ -6,12 +6,17 @@ Template.profileDetails.helpers({
   userInfo: function(){
     var userId = FlowRouter._current.params.id
     var user = Meteor.users.findOne({_id: userId})
-    console.log(user.profile.images[0].url);
     return user
   },
   userPlaylists: function(){
     var userId = FlowRouter._current.params.id
     var playlists = Playlists.find({userId: userId}).fetch();
     return playlists
+  },
+  userImage: function(){
+    var userId = FlowRouter._current.params.id
+    var user = Meteor.users.findOne({_id: userId})
+    var image = user.profile.images[0].url
+    return image
   }
 });
