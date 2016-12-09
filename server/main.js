eda5b6a04ec75dff897df80441f2f5bfacd07588
James <James.Steel@me.com>
2016-12-09 18:18:22 +0000

    user can now update playlist, adds tracks to DB that they added to playlist after initial import

--- server/main.js
+++ server/main.js
@@ -24,14 +24,14 @@ Meteor.methods({
     if (checkTokenRefreshed(response, spotifyApi)) {
       var response = spotifyApi.getUserPlaylists(Meteor.user().services.spotify.id, {limit: 50})
     }
-    console.log(response);
     return response.data.body.items
   },
 
   insertPlaylists: function(newPlaylists){
     newPlaylists.forEach(function(playlist){
       if (Playlists.findOne({userId: Meteor.userId(), spotifyId: playlist.id})){
-       return;
+        var existingPlaylist = Playlists.findOne({userId: Meteor.userId(), spotifyId: playlist.id});
+        Meteor.call('updatePlaylist', playlist);
       } else {
         var playlistId = Playlists.insert({
           userId: Meteor.userId(),
@@ -73,7 +73,54 @@ Meteor.methods({
         playlistId: playlistId,
       });
     });
+  },
+
+  updatePlaylist: function(existingPlaylist){
+    console.log(existingPlaylist.name)
+      Playlists.update({ spotifyId: existingPlaylist.id },{$set:{
+      userId: Meteor.userId(),
+      spotifyId: existingPlaylist.id,
+      name: existingPlaylist.name,
+      trackCount: existingPlaylist.tracks.total,
+      ownerId: existingPlaylist.owner.id,
+    }});
+     var playlistId = Playlists.findOne({userId: Meteor.userId(), spotifyId: existingPlaylist.id})
+     Meteor.call('getUpdatedTracks', playlistId)
+  },
+
+  getUpdatedTracks: function(playlistId){
+    var playlist = Playlists.findOne(playlistId)
+    var spotifyApi = new SpotifyWebApi()
+    var response = spotifyApi.getPlaylistTracks(playlist.ownerId, playlist.spotifyId, {})
+    if (checkTokenRefreshed(response, spotifyApi)) {
+      var response = spotifyApi.getPlaylistTracks(playlist.ownerId, playlist.spotifyId, {})
+    }
+    var trackObjects = response.data.body.items
+    Meteor.call('updateTracks', trackObjects, playlistId)
+  },
+
+  updateTracks: function(trackObjects, playlistId){
+    trackObjects.forEach(function(trackObject){
+      var track = trackObject.track;
+      var artistObjects = []
+      track.artists.forEach(function(artist){
+        artistObjects.push({name: artist.name, spotifyId: artist.id})
+      });
+      if(Tracks.findOne({userId: Meteor.userId(), 'playlistId.id': playlistId.id, spotifyId: track.id})){
+        return;
+      } else {
+      Tracks.insert({
+        userId: Meteor.userId(),
+        name: track.name,
+        artists: artistObjects,
+        spotifyId: track.id,
+        playlistId: playlistId,
+      });
+     }
+    });
   }
+
+
 
 });
 
