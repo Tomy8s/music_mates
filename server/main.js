0fb8202341f8242fe97a6e0e6b68d4320f247606
Tadas Majeris <tadas.majeris@gmail.com>
2016-12-12 11:44:16 +0000

    refactored updating tracks. removes tracks if deleted from spotify

--- server/main.js
+++ server/main.js
@@ -29,9 +29,9 @@ Meteor.methods({
 
   insertPlaylists: function(newPlaylists){
     newPlaylists.forEach(function(playlist){
-      if (Playlists.findOne({userId: Meteor.userId(), spotifyId: playlist.id})){
-        var existingPlaylist = Playlists.findOne({userId: Meteor.userId(), spotifyId: playlist.id});
-        Meteor.call('updatePlaylist', playlist);
+      var existingPlaylist = Playlists.findOne({userId: Meteor.userId(), spotifyId: playlist.id});
+      if (existingPlaylist){
+        Meteor.call('updatePlaylist', existingPlaylist._id, playlist);
       } else {
         var playlistId = Playlists.insert({
           userId: Meteor.userId(),
@@ -65,62 +65,50 @@ Meteor.methods({
       track.artists.forEach(function(artist){
         artistObjects.push({name: artist.name, spotifyId: artist.id})
       });
-      Tracks.insert({
-        userId: Meteor.userId(),
-        name: track.name,
-        artists: artistObjects,
-        spotifyId: track.id,
-        playlistId: playlistId,
-      });
-    });
-  },
 
-  updatePlaylist: function(existingPlaylist){
-    console.log(existingPlaylist.name)
-      Playlists.update({ spotifyId: existingPlaylist.id },{$set:{
-      userId: Meteor.userId(),
-      spotifyId: existingPlaylist.id,
-      name: existingPlaylist.name,
-      trackCount: existingPlaylist.tracks.total,
-      ownerId: existingPlaylist.owner.id,
-    }});
-     var playlistId = Playlists.findOne({userId: Meteor.userId(), spotifyId: existingPlaylist.id})
-     Meteor.call('getUpdatedTracks', playlistId)
-  },
-
-  getUpdatedTracks: function(playlistId){
-    var playlist = Playlists.findOne(playlistId)
-    var spotifyApi = new SpotifyWebApi()
-    var response = spotifyApi.getPlaylistTracks(playlist.ownerId, playlist.spotifyId, {})
-    if (checkTokenRefreshed(response, spotifyApi)) {
-      var response = spotifyApi.getPlaylistTracks(playlist.ownerId, playlist.spotifyId, {})
-    }
-    var trackObjects = response.data.body.items
-    Meteor.call('updateTracks', trackObjects, playlistId)
-  },
-
-  updateTracks: function(trackObjects, playlistId){
-    trackObjects.forEach(function(trackObject){
-      var track = trackObject.track;
-      var artistObjects = []
-      track.artists.forEach(function(artist){
-        artistObjects.push({name: artist.name, spotifyId: artist.id})
-      });
-      if(Tracks.findOne({userId: Meteor.userId(), 'playlistId.id': playlistId.id, spotifyId: track.id})){
+      if(Tracks.findOne({userId: Meteor.userId(), playlistId: playlistId, spotifyId: track.id})){
         return;
       } else {
-      Tracks.insert({
-        userId: Meteor.userId(),
-        name: track.name,
-        artists: artistObjects,
-        spotifyId: track.id,
-        playlistId: playlistId,
-      });
-     }
+        Tracks.insert({
+          userId: Meteor.userId(),
+          name: track.name,
+          artists: artistObjects,
+          spotifyId: track.id,
+          playlistId: playlistId,
+        });
+      }
     });
+
+    Meteor.call('removeDeletedTracks', trackObjects, playlistId);
+  },
+
+  updatePlaylist: function(playlistId, spotifyPlaylist){
+    Playlists.update(playlistId, {$set:{
+      trackCount: spotifyPlaylist.tracks.total
+    }});
+
+    Meteor.call('findTracks', playlistId)
+  },
+
+  // deletes tracks from database if they were removed in Spotify
+  removeDeletedTracks: function(trackObjects, playlistId) {
+    var spotifyTracksIds = [];
+    trackObjects.forEach(function(trackObj){
+      spotifyTracksIds.push(trackObj.track.id);
+    });
+
+    var tracks = Tracks.find({playlistId: playlistId}).fetch();
+    var trackIdsInDatabase = [];
+    tracks.forEach(function(track){
+      trackIdsInDatabase.push(track.spotifyId);
+    });
+
+    var deletedTracksIds = _.difference(trackIdsInDatabase, spotifyTracksIds);
+
+    deletedTracksIds.forEach(function(id){
+      Tracks.remove({playlistId: playlistId, spotifyId: id});
+    })
   }
-
-
 
 });
 
