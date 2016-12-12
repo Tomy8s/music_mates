4ed35ec6aa9022836116b1c5b6aa6ac0be00fba2
Thomas Yates <tomy8s@hotmail.com>
2016-12-12 13:26:42 +0000

    Revert "Fixing import playlists/remove and add songs"

--- client/MyPlaylists/MyPlaylists.js
+++ client/MyPlaylists/MyPlaylists.js
@@ -2,14 +2,8 @@ import { SpotifyWebApi } from 'meteor/xinranxiao:spotify-web-api';
 
 Template.MyPlaylists.events({
   'click #import_playlists'(event){
-    document.getElementById('playlist_loading_gif').style='display: unset';
-    Meteor.call('getPlaylists', function(error, returnedPlaylists) {
-      Meteor.call('insertPlaylists', returnedPlaylists, function(error, playlists){
-        document.getElementById('playlist_loading_gif').style='display: none';
-        playlists.forEach(function(playlist){
-          Meteor.call('findTracks', playlist._id)
-        });
-      });
+    Meteor.call('getPlaylists', function(error, result) {
+      Meteor.call('insertPlaylists', result);
     })
   },
   'click .playlist'(event){
