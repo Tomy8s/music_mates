1a9a320f8a13ce7c943244c98c3273d3d9e746a0
Tomy8s <tomy8s@hotmail.com>
2016-12-13 15:01:18 +0000

    Added test for displaying message to a user who sends it

--- features/messages.test.js
+++ features/messages.test.js
@@ -1,4 +1,4 @@
-import { signUp, signIn, signOut,login, cleanDatabase, createAccount } from './testHelpers.test'
+import { signUp, signIn, signOut, login, cleanDatabase, createAccount, makeAndAcceptFriendRequest } from './testHelpers.test'
 
 describe('Messages', function(){
 
@@ -10,25 +10,27 @@ describe('Messages', function(){
   describe('No messages', function(){
    it('displays a message if you have no converstations', function(){
      browser.url('http://localhost:3100/messages');
-     browser.pause(200)
+     browser.pause(200);
      expect(browser.getText('#messages-list')).to.equal('You currently have no conversations.');
    });
 
-   it('displays a list of your friends on the page @watch', function(){
-     signOut();
-     signUp('user2', 'email2@email.com', 'password');
-     browser.waitForExist('#friend-request-btn', 5000);
-     browser.click('#friend-request-btn');
-     signOut();
-     signIn('user', 'password');
+   it('displays a list of your friends on the page', function(){
+     makeAndAcceptFriendRequest();
+     browser.url('http://localhost:3100/messages');
      browser.pause(200);
-     browser.url('http://localhost:3100/friends');
-     browser.waitForExist('#requests-received-list', 5000);
-     browser.click('#requests-received-list .accept-friend-request');
-     browser.url('http://localhost:3100/messages')
-     browser.pause(200)
      expect(browser.getText('#messages-friends-list')).to.equal('user2: Send message');
    });
   });
 
+  it('shows messages a user has sent on their messages page @watch', function(){
+    makeAndAcceptFriendRequest();
+    browser.url('http://localhost:3100/messages');
+    browser.waitForExist('#messages-friends-list');
+    browser.click('#messages-friends-list .send-message-link');
+    browser.waitForExist('#message-input');
+    var msg = 'Hello User2';
+    browser.setValue('#message-input', msg);
+    browser.keys("\uE006"); //press ENTER
+    expect(browser.getText('#chat .message:first-child')).to.equal(msg);
+  });
 });
