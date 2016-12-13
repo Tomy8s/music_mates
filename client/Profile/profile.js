811beaece60247fceaa609b9d545e0ac057ef7ae
Tadas Majeris <tadas.majeris@gmail.com>
2016-12-13 17:45:17 +0000

    Fixed exception if user doesnt have a spotify image

--- client/Profile/profile.js
+++ client/Profile/profile.js
@@ -1,22 +1,24 @@
-Template.Profile.rendered = function(){
-};
-
-
 Template.profileDetails.helpers({
+
   userInfo: function(){
     var userId = FlowRouter._current.params.id
     var user = Meteor.users.findOne({_id: userId})
     return user
   },
+
   userPlaylists: function(){
     var userId = FlowRouter._current.params.id
     var playlists = Playlists.find({userId: userId}).fetch();
     return playlists
   },
+
   userImage: function(){
-    var userId = FlowRouter._current.params.id
-    var user = Meteor.users.findOne({_id: userId})
-    var image = user.profile.images[0].url
-    return image
+    var userId = FlowRouter._current.params.id;
+    var user = Meteor.users.findOne(userId);
+    if (user.profile.images.length > 0) {
+      var image = user.profile.images[0].url
+      return image
+    }
   }
+
 });
