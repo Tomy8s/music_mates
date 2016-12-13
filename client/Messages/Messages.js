25cea9b8fef4629c3ad13fd2f9db3a7198cd002b
Tomy8s <tomy8s@hotmail.com>
2016-12-13 14:03:49 +0000

    Test if broken

--- client/Messages/Messages.js
+++ client/Messages/Messages.js
@@ -1,1 +1,8 @@
 import { Template } from 'meteor/templating';
+
+Template.messages.helpers({
+    hasFriends: function() {
+        // console.log(Meteor.user().friends());
+        return Meteor.user().friends().count() === 0 ? false : true;
+    }
+});
