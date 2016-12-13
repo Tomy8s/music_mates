a5db0fef53e4d8cc20fab0d07e5480eac94c597e
James <James.Steel@me.com>
2016-12-13 10:40:21 +0000

    messages started, one test to pass

---
+++ features/messages.test.js
@@ -0,0 +1,18 @@
+import { signUp, signIn, signOut,login, cleanDatabase, createAccount } from './testHelpers.test'
+
+describe('Messages', function(){
+
+  beforeEach(function() {
+    cleanDatabase();
+    signUp('user', 'email@email.com', 'password');
+  });
+
+  describe('No messages', function(){
+   it('displays a message if you have no converstations', function(){
+   browser.url('http://localhost:3100/messages');
+   browser.pause(200)
+   expect(browser.getText('#message-list')).to.equal('You currently have no converstations');
+   });
+  });
+
+});
