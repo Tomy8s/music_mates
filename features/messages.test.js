4954739012b7363fc432a4a79aa7448dc3990e44
James <James.Steel@me.com>
2016-12-13 13:26:02 +0000

    test failing for list of freinds on message page

--- features/messages.test.js
+++ features/messages.test.js
@@ -9,9 +9,25 @@ describe('Messages @watch', function(){
 
   describe('No messages', function(){
    it('displays a message if you have no converstations', function(){
-   browser.url('http://localhost:3100/messages');
-   browser.pause(200)
-   expect(browser.getText('#message-list')).to.equal('You currently have no conversations.');
+     browser.url('http://localhost:3100/messages');
+     browser.pause(200)
+     expect(browser.getText('#message-list')).to.equal('You currently have no conversations.');
+   });
+
+   it('displays a list of your friends on the page @watch', function(){
+     signOut();
+     signUp('user2', 'email2@email.com', 'password');
+     browser.waitForExist('#friend-request-btn', 5000);
+     browser.click('#friend-request-btn');
+     signOut();
+     signIn('user', 'password');
+     browser.pause(200);
+     browser.url('http://localhost:3100/friends');
+     browser.waitForExist('#accept-friend-request', 5000);
+     browser.click('#accept-friend-request');
+     browser.url('http://localhost:3100/messages')
+     browser.pause(200)
+     expect(browser.getText('#messages-friend-list')).to.equal('user: Send message');
    });
   });
 
