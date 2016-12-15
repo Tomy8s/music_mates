8b5fb4ae542526331008aca0415c85092b7bd435
souljuse <oppo.irene@gmail.com>
2016-12-15 19:34:23 +0100

    notifications that seem to work

--- client/layouts/Navigation.js
+++ client/layouts/Navigation.js
@@ -10,11 +10,22 @@ Template.Navigation.events({
     var currentlySelectedButton = document.getElementsByClassName('selected_route')[0];
     currentlySelectedButton.className = 'navButton';
     document.getElementById(buttonId).className = 'selected_route';
-  }
+  },
 });
 
 Template.MainLayout.events({
   'click .backLink'(){
     history.back()
+  },
+  'click #notificationsCounter'(){
+    FlowRouter.go(`/messages`);
   }
+});
+
+Template.MainLayout.helpers({
+  messageCounter: function() {
+    var newMessages = Notifications.find({to: Meteor.user()._id}).fetch();
+    return newMessages.length;
+  },
+
 });
